5943cb7fe3b043ad60ab0683fd0b6bd2c19c141c
nicolasSagon <sagon.nicolas@gmail.com>
2025-08-04 15:29:21 +0200

    feat: add decryption with encryption key from s3

--- src/dossierfacile_file_analysis/services/file_downloader/s3_file_downloader.py
+++ src/dossierfacile_file_analysis/services/file_downloader/s3_file_downloader.py
@@ -1,3 +1,5 @@
+import base64
+import hashlib
 import os
 import time
 
@@ -19,21 +21,38 @@ class S3FileDownloader(FileDownloader):
         self.s3_client = boto3.client(
             "s3",
             endpoint_url=os.getenv("S3_ENDPOINT_URL"),
-            aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
-            aws_secret_access_key=os.getenv("S3_SECRET_KEY"),
+            aws_access_key_id=os.getenv("S3_FILE_ANALYSIS_ACCESS_KEY"),
+            aws_secret_access_key=os.getenv("S3_FILE_ANALYSIS_SECRET_KEY"),
             config=Config(signature_version="s3v4"),
             region_name=os.getenv("S3_REGION")
         )
-        self.bucket_name = os.getenv("S3_BUCKET")
+        self.bucket_name = os.getenv("S3_RAW_BUCKET_NAME")
 
     def download_file(self, file_dto: FileDto):
         logger.info("Downloading file from s3 storage")
         start_time = time.time()
         try:
             output_path = f"{self.local_file_path}{file_dto.get_system_name()}"
-            self.s3_client.download_file(self.bucket_name, file_dto.path, output_path)
+
+            # From the file_dto, we get the encryption key and compute the necessary headers for SSE-C
+            sse_customer_key = base64.b64encode(file_dto.encryption_key).decode('utf-8')
+            md5_hash = hashlib.md5(file_dto.encryption_key).digest()
+            sse_customer_key_md5 = base64.b64encode(md5_hash).decode('utf-8')
+
+            with open(output_path, 'wb') as data:
+                self.s3_client.download_fileobj(
+                    self.bucket_name,
+                    file_dto.path,
+                    data,
+                    ExtraArgs={
+                        "SSECustomerAlgorithm": "AES256",
+                        "SSECustomerKey": sse_customer_key,
+                        "SSECustomerKeyMD5": sse_customer_key_md5
+                    })
+
         except Exception as e:
             raise RetryableException("Failed to download file from OVH storage") from e
         end_time = time.time()
         logger.info(f"download file take : {end_time - start_time:.2f} seconds")
-        return DownloadedFile(file_name=file_dto.path, file_path=output_path, file_type=file_dto.content_type)
+        return DownloadedFile(file_name=file_dto.get_system_name(), file_path=output_path,
+                              file_type=file_dto.content_type)
