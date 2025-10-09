f36d67bef9e85f056722730a55e3799a7ef19c26
nicolasSagon <sagon.nicolas@gmail.com>
2025-10-09 10:48:38 +0200

    fix: add a safe wrapper to amqp call crash rabbit no connexion

--- src/dossierfacile_file_analysis/services/arg_provider_service.py
+++ src/dossierfacile_file_analysis/services/arg_provider_service.py
@@ -3,21 +3,25 @@ import os
 
 
 class ArgProviderService:
-    def __init__(self):
-        parser = argparse.ArgumentParser()
+    def __init__(self, argv=None):
+        parser = argparse.ArgumentParser(add_help=False)
         parser.add_argument("--thread-number", type=str, help="Nombre de threads")
-        args = parser.parse_args()
+        try:
+            # parse_known_args prevent crash with pytest when he pass unexpected args
+            args, _ = parser.parse_known_args(argv)
+        except SystemExit:
+            args = argparse.Namespace(thread_number=None)
 
-        if args.thread_number:
-            os.environ["THREAD_NUMBER"] = args.thread_number
-        else:
-            os.environ["THREAD_NUMBER"] = "4"
+        # Use command line argument if provided, otherwise fallback to environment variable or default to "4"
+        thread_number = args.thread_number or os.getenv("THREAD_NUMBER") or "4"
+        os.environ["THREAD_NUMBER"] = str(thread_number)
 
     def get_thread_number(self) -> int:
-        """Retourne le nombre de threads configuré."""
+        """Return the number of thread to use."""
         thread_number = os.getenv("THREAD_NUMBER")
         if thread_number is None:
             raise ValueError("THREAD_NUMBER environment variable is not set.")
         return int(thread_number)
+
 
 arg_provider_service = ArgProviderService()
